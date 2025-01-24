4ab31ce0ad98204c006ecadeb2306bd2e9669978
Harshitha Srinivas <55800824+Harshitha-Srinivas29@users.noreply.github.com>
2025-01-23 21:49:30 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server  } from './socket/socket.js';
-
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 
@@ -25,7 +25,7 @@ app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
-
+app.use(cors()); 
 //app.get('*', (req, res) => {res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
 // });
 
